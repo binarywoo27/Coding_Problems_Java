ca30069a351cec38cf0b1e5f036a8d01915a6e4a
이진우 21400603 <49421143+binarywoo27@users.noreply.github.com>
2022-01-08 07:05:41 +0000

    문자열에서 특정 문자 개수 세기

--- Grammar/Count_NoOfOccurrences/Main.java
+++ Grammar/Count_NoOfOccurrences/Main.java
@@ -2,26 +2,29 @@ import java.util.Scanner;
 import java.lang.String;
 
 public class Main { // 클래스 : new 를 통해 인스턴스 생성 
-	public String Solution(String str) { // 인스턴스 메소드 
+	public int Solution(String str, char c) { // 인스턴스 메소드 
 		
-		String ans = "";
+		// convert all str to uppercase
+		// loop the str and count the number of char 
+		int cnt = 0;
+		str.toUpperCase();
 		for(int i=0; i<str.length(); i++) {
-			//if(i.equals(str.indexOf(str.charAt(i))))
-			if(str.indexOf(str.charAt(i)) == i) ans += str.charAt(i);
+			if(c == str.charAt(i)) cnt++;
 		}
-		//System.out.println(ans);
-		return ans;
+		return cnt;		
 	};
+	
 	int a = 1;
 	
 	public static void main(String[] args) { // 클래스 메소드 
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
+		char c = sc.next().charAt(0);
 		
 		Main sol = new Main(); // 클래스 메소드(static이 붙으면)에는 인스턴스 메소드를 사용하기 위해선
 		// 클래스 객체를 생성해야한다. 
 		// 즉, Solution 함수는 클래스가 아니고, 클래스 안에 있는 함수라 Main 객체(인스턴스)를 생성해야 사용할 수 있음
-		String ans = sol.Solution(str);
+		int ans = sol.Solution(str, c);
 		System.out.println(ans);
 	}
 }
